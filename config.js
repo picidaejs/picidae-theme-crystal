1d7ce8d596ce48d7b3905ec7035bac9a466f232b
Claiyre_ <mayingying1996@163.com>
2018-01-17 23:19:42 +0800

    improve the position of footer and make the background of favicon.png transparent

--- config.js
+++ config.js
@@ -3,5 +3,13 @@ module.exports = {
   pageSize: 6 ,
   title: 'Claiyre的个人博客',
   blogName: false,           // the text in the header of blog pages
-  avatarUrl: 'avatar.png'   // put your avatar image under ~./extra/ 
+  avatarUrl: 'avatar.png',   // put your avatar image under ~./extra/ 
+  gitment: {
+    owner: 'Claiyre',
+    repo: 'Claiyre.github.io',
+    oauth: {
+      client_id: 'xxx',
+      client_secret: 'xxxx'
+    }
+  }
 }
