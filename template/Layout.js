99750a123bb63cfc265475c0de0c9873b7a1303f
Claiyre_ <mayingying1996@163.com>
2018-02-23 19:48:05 +0800

    add busuanzi

--- template/Layout.js
+++ template/Layout.js
@@ -6,12 +6,18 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 class Layout extends React.Component {
-
+    componentDidMount() {
+        const s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = "//dn-lbstatics.qbox.me/busuanzi/2.3/busuanzi.pure.mini.js";
+        this.instance.appendChild(s);
+    }
     render() {
       console.log('layout props:', this.props);
         const {publicPath, pluginData: {utils}, themeConfig} = this.props;
         return (
-            <div className="wrap">
+            <div className="wrap" ref={el => (this.instance = el)}>
                 <Header publicPath={publicPath} blogName={themeConfig.blogName}/>
                 <main>{this.props.children}</main>
                 <Footer publicPath={publicPath} avatarUrl={themeConfig.avatarUrl}/>
--- template/components/Footer.js
+++ template/components/Footer.js
@@ -10,4 +10,12 @@ export default ({publicPath, avatarUrl}) =>
     <div className="copyright">
       © <time>{thisYear}</time>. Powered By&nbsp;<a href="https://github.com/picidaejs/picidae"> Picidae </a>&nbsp; |&nbsp; <a href="https://github.com/picidaejs/picidae-theme-crystal"> theme-crystal</a> 
     </div>
+    <div id="busuanzi-count">
+      <span id="busuanzi_container_site_uv">
+        <span className="icon-font">&#xe642;</span><span id="busuanzi_value_site_uv"></span>
+      </span>&nbsp;&nbsp;|&nbsp;&nbsp;
+      <span id="busuanzi_container_site_pv">
+       <span className="icon-font">&#xe680;</span>&nbsp;<span id="busuanzi_value_site_pv"></span>
+      </span>
+    </div>
   </footer>
